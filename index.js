8c89e77655db57a1d7b03f6379fc351e37f5f8d2
Cora4047 <corahelsden@gmail.com>
2026-03-04 04:23:26 +0000

    Added generic interface design

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// =============================================
+//                 BASE STUFF
+// =============================================
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
 
@@ -9,6 +12,59 @@ const names = [
     "big v"
 ];
 
+// =============================================
+//             INTERFACE STUFF
+// =============================================
+
+let phase = 0;
+let zoff = 0;
+let circles = [];
+
+function setup() {
+    createCanvas(window.screen.width, window.screen.height);
+
+    let totalCircles = 10;
+
+    for (let i = 0; i < totalCircles; i++) {
+        circles.push({ noiseOffset: random(1000), scale: random(2, 5) });
+    }
+
+    stroke(255);
+    strokeWeight(2);
+    noFill();
+}
+
+function draw() {
+    background(0);
+    translate(width / 2, height / 2);
+
+    for (let i = 0; i < circles.length; i++) {
+        let c = circles[i];
+        beginShape();
+
+        let minRadius = 10 + i * 15;
+        let maxRadius = 140 + i * 15;
+
+        for (let a = 0; a < TWO_PI; a += radians(0.5)) {
+            let xoff = map(cos(a + phase), -1, 1, 0, c.scale);
+            let yoff = map(sin(a + phase), -1, 1, 0, c.scale);
+            let r = map(noise(xoff + c.noiseOffset, yoff + c.noiseOffset, zoff - 10), 0, 1, minRadius, maxRadius);
+            let x = r * cos(a);
+            let y = r * sin(a);
+            vertex(x, y);
+        }
+        endShape(CLOSE);
+    }
+
+    phase += 0.003;
+    zoff += 0.01;
+}
+
+
+
+// =============================================
+//             ACTUAL VOICE STUFF
+// =============================================
 const recognition = new SpeechRecognition();
 
 recognition.continuous = true; // temp
@@ -17,6 +73,7 @@ recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
 const startBtn = document.querySelector("button")
+const test = document.querySelector("p")
 
 startBtn.onclick = () => {
     recognition.start();
@@ -28,6 +85,10 @@ recognition.onresult = (event) => {
     console.log("Caught input!");
     console.log(`Result: ${inp}`)
     console.log(`Confidence: ${event.results[0][0].confidence}`);
+
+    if (inp == "What is the time?") {
+        console.log("the time is...");
+    }
 }
 
 recognition.onspeechend = () => {
@@ -41,3 +102,4 @@ recognition.onnomatch = (event) => {
 recognition.onerror = (event) => {
     console.log(`Error occurred: ${event.error}`);
 }
+
